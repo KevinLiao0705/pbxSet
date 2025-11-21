0157a5ba2b3201915b90102df8486aa6d82a56c1
Kevin Liao <kevin.josn@gmial.com>
2025-11-21 17:27:52 +0800

    edit

--- src/base3/GB.java
+++ src/base3/GB.java
@@ -29,8 +29,8 @@ public class GB {
     //2: linux ConsoleMain
     //3: linux ConsoleSlot
     static int prgMode = 0;
-    static String webSocketIp="";
-    //static String webSocketIp="192.168.0.28";
+    //static String webSocketIp="";
+    static String webSocketIp="192.168.0.128";
     static String version = "3.2";
     
     
